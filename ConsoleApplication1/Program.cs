62e3a98b50242cc31243f9545416590ae6df8232
kiwamu <kiwamu@KIWAMU-PC>
2016-07-11 12:55:01 +0900

    ２人じゃんけんです。

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    class program1
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int player;
+            int cpu;
+
+            string temp1;
+
+            Console.WriteLine("じゃんけんゲーム");
+            Console.WriteLine("1.グー, 2.チョキ, 3.パー");
+            Console.WriteLine("1～3のいずれかを選択してください>>> ");
+
+            do
+            {
+
+                temp1 = Console.ReadLine();
+                player = int.Parse(temp1);
+
+                Random temp2 = new Random();
+                cpu = temp2.Next(1, 4);
+
+                while (player == cpu)
+                {
+                    Console.WriteLine("あいこです ( 'ω' )");
+                    Console.WriteLine("もう一度、1.グー, 2.チョキ, 3.パーから選択してください>>> ");
+                    temp1 = Console.ReadLine();
+                    player = int.Parse(temp1);
+                    Random temp3 = new Random();
+                    cpu = temp3.Next(1, 4);
+                }
+
+                if (player == 1 && cpu == 2 || player == 2 && cpu == 3 || player == 3 && cpu == 1)
+                {
+                    Console.WriteLine("あなたの勝ちですヽ(〃v〃)ﾉ");
+                    Console.WriteLine("もう一度やるときは、1.グー, 2.チョキ, 3.パーから選択してください>>> ");
+                    Console.WriteLine("終了するときは、4を選択してください>>>");
+                }
+                else if (player == 1 && cpu == 3 || player == 2 && cpu == 1 || player == 3 && cpu == 2)
+                {
+                    Console.WriteLine("あなたの負けです (_　_|||)");
+                    Console.WriteLine("もう一度やるときは、1.グー, 2.チョキ, 3.パーから選択してください>>> ");
+                    Console.WriteLine("終了するときは、4を選択してください>>>");
+                   
+                }
+            } while (player == 1 || player == 2 || player == 3);
         }
     }
 }
+
+
